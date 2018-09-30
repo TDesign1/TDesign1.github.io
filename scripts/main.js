cc10a6435c4a78e84809cf0333a2ab9fe810f374
Tim <trdavies3@gmail.com>
2018-09-30 17:55:35 +0100

    newsite

---
+++ scripts/main.js
@@ -0,0 +1,87 @@
+$("#wrapper").click( function() {
+	$(".menu").toggleClass("close");
+});
+
+
+$(document).ready(function(){
+$("#wedspan").hover(function(){
+    /* 
+    Hover alows a mouseenter and mouse leave event
+    seperated by a comma
+    */
+    
+    //Event one mouse enter add class
+    $("#overlaywed").addClass("hoverstate");
+  },
+    // Event two mouse out remove class               
+    function(){
+    $("#overlaywed").removeClass("hoverstate");
+  });
+});
+
+
+$(document).ready(function(){
+	$("#contactspan").hover(function(){
+		/* 
+		Hover alows a mouseenter and mouse leave event
+		seperated by a comma
+		*/
+		
+		//Event one mouse enter add class
+		$("#contactlay").addClass("hoverstate");
+	  },
+		// Event two mouse out remove class               
+		function(){
+		$("#contactlay").removeClass("hoverstate");
+	  });
+	});
+
+	$(document).ready(function(){
+		$("#shopspan").hover(function(){
+			/* 
+			Hover alows a mouseenter and mouse leave event
+			seperated by a comma
+			*/
+			
+			//Event one mouse enter add class
+			$("#shoplay").addClass("hoverstate");
+		  },
+			// Event two mouse out remove class               
+			function(){
+			$("#shoplay").removeClass("hoverstate");
+		  });
+		});
+
+		$(document).ready(function(){
+			$("#portspan").hover(function(){
+				/* 
+				Hover alows a mouseenter and mouse leave event
+				seperated by a comma
+				*/
+				
+				//Event one mouse enter add class
+				$("#portlay").addClass("hoverstate");
+			  },
+				// Event two mouse out remove class               
+				function(){
+				$("#portlay").removeClass("hoverstate");
+			  });
+			});
+
+			$(document).ready(function(){
+				$("#wedspan").hover(function(){
+					/* 
+					Hover alows a mouseenter and mouse leave event
+					seperated by a comma
+					*/
+					
+					//Event one mouse enter add class
+					$("#overlaywed").addClass("hoverstate");
+				  },
+					// Event two mouse out remove class               
+					function(){
+					$("#overlaywed").removeClass("hoverstate");
+				  });
+				});
+				
+
